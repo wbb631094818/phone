9cc7c72b56f407f687a452f261294b19b8909f4d
charles <charles@everzones.com>
2018-04-17 10:41:57 +0800

    Initial commit

---
+++ app/src/main/java/com/phone/phone/MainActivity.java
@@ -0,0 +1,43 @@
+package com.phone.phone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.phone.libphone.NumberParseException;
+import com.phone.libphone.PhoneNumberOfflineGeocoder;
+import com.phone.libphone.PhoneNumberToCarrierMapper;
+import com.phone.libphone.PhoneNumberUtil;
+import com.phone.libphone.Phonenumber;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String swissNumberStr = "14783359524";
+        PhoneNumberUtil phoneUtil = PhoneNumberUtil.createInstance(this);
+        try {
+            Phonenumber.PhoneNumber swissNumberProto = phoneUtil.parse(swissNumberStr, "US");
+            String str = phoneUtil.format(swissNumberProto, PhoneNumberUtil.PhoneNumberFormat.INTERNATIONAL);
+            PhoneNumberOfflineGeocoder geocoder = PhoneNumberOfflineGeocoder.getInstance(this);
+// Outputs "Zurich"
+            String Description = geocoder.getDescriptionForNumber(swissNumberProto, Locale.ENGLISH);
+
+            Phonenumber.PhoneNumber swissMobileNumber = new Phonenumber.PhoneNumber().setCountryCode(1).setNationalNumber(14783359524L);
+            PhoneNumberToCarrierMapper carrierMapper = PhoneNumberToCarrierMapper.getInstance(this);
+// Outputs "Swisscom"
+            String carrier = carrierMapper.getNameForNumber(swissMobileNumber, Locale.ENGLISH);
+
+            Log.e("wbb", "号码格式化： " + str);
+            Log.e("wbb","产地:  "+Description);
+            Log.e("wbb","运营商:  "+carrier);
+            Log.e("wbb","号码类型:  "+phoneUtil.getNumberType(swissNumberProto));
+        } catch (NumberParseException e) {
+            Log.e("wbb", "NumberParseException was thrown: " + e.toString());
+        }
+    }
+}
